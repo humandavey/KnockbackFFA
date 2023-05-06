651e7aaf02d1a5b61ee03d5fbecc529da8119fa2
humandavey <davey.adams.three@gmail.com>
2023-05-05 21:26:09 -0500

    Map object

---
+++ src/main/java/me/humandavey/knockbackffa/map/KnockbackMap.java
@@ -0,0 +1,45 @@
+package me.humandavey.knockbackffa.map;
+
+import org.bukkit.Location;
+
+public class KnockbackMap {
+
+	private String name;
+	private Location spawn, pos1, pos2;
+
+	public KnockbackMap(String name, Location spawn) {
+		this.name = name;
+		this.spawn = spawn;
+	}
+
+	public KnockbackMap(String name, Location spawn, Location pos1, Location pos2) {
+		this.name = name;
+		this.spawn = spawn;
+		this.pos1 = pos1;
+		this.pos2 = pos2;
+	}
+
+	public boolean isMapReady() {
+		return pos1 != null && pos2 != null;
+	}
+
+	public boolean isLocationInBounds(Location location) {
+		int minX = Math.min(pos1.getBlockX(), pos2.getBlockX());
+		int minY = Math.min(pos1.getBlockY(), pos2.getBlockY());
+		int minZ = Math.min(pos1.getBlockZ(), pos2.getBlockZ());
+
+		int maxX = Math.max(pos1.getBlockX(), pos2.getBlockX());
+		int maxY = Math.max(pos1.getBlockY(), pos2.getBlockY());
+		int maxZ = Math.max(pos1.getBlockZ(), pos2.getBlockZ());
+
+		return (location.getX() >= minX && location.getX() <= maxX) && (location.getY() >= minY && location.getY() <= maxY) && (location.getZ() >= minZ && location.getZ() <= maxZ);
+	}
+
+	public Location getSpawn() {
+		return spawn;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
